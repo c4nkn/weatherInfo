a5c55165efd7b41ec9a6f0a254090addf56fb43a
c4nkn <cankahraman05@gmail.com>
2023-12-04 02:51:07 +0300

    Added API request methods simply.

--- weatherInfo/ApiHandler.cs
+++ weatherInfo/ApiHandler.cs
@@ -1,10 +1,53 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace weatherInfo
 {
 	public class ApiHandler
 	{
+		private static string apiUrls { get; set; }
 		
+		public static async Task<WeatherInfo> requestTo(string apiUrl)
+		{
+			try
+			{
+				HttpClient client = new HttpClient();
+				HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+				if (response.IsSuccessStatusCode)
+				{
+					string responseData = await response.Content.ReadAsStringAsync();
+					WeatherInfo weatherInfo = JsonConvert.DeserializeObject<WeatherInfo>(responseData);
+					return weatherInfo;
+				}
+				else
+				{
+					return null;
+				}
+			}
+			catch (HttpRequestException exc)
+			{
+				Console.WriteLine(exc);
+				return null;
+			}
+		}
+		
+		public static async Task<WeatherInfo> getDataFor(string city)
+		{
+			string[] cities = { "istanbul", "izmir", "ankara" };
+
+			if (cities.Contains(city.ToLower()))
+			{
+				apiUrls = $"https://goweather.herokuapp.com/weather/{city}";
+				WeatherInfo weatherInfo = await requestTo(apiUrls);
+				return weatherInfo;
+			}
+			else
+			{
+				Console.WriteLine($"Couldn't find data for {city}");
+				return null;
+			}
+		}
 	}
 }
 
