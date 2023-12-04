6773097a18cb15d737ce154be234452517960579
c4nkn <cankahraman05@gmail.com>
2023-12-04 03:00:56 +0300

    Added simple CLI things to test app.

--- weatherInfo/Program.cs
+++ weatherInfo/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using weatherInfo;
 
+Console.Write("Enter city: ");
+string city = Console.ReadLine();
+
+var weatherData = await ApiHandler.getDataFor(city);
+Console.WriteLine($"Showing weather information for: {city}");
+Console.WriteLine($"Description: {weatherData.description}\nTemperature: {weatherData.temperature}\nWind: {weatherData.wind}");
+
+foreach (var forecast in weatherData.forecast)
+{
+    Console.WriteLine($">> Day: {forecast.day}, Temperature: {forecast.temperature}, Wind: {forecast.wind}");
+}
